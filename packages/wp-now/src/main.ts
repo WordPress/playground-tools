41bbb0dda47399ba65bfdd27a976b935dfb8bbcb
Antonio Sejas <antonio@sejas.es>
2023-05-31 19:16:06 +0000

    wp-now: avoid killing the app for lower node versions

--- packages/wp-now/src/main.ts
+++ packages/wp-now/src/main.ts
@@ -2,13 +2,12 @@ import { runCli } from './run-cli';
 
 const requiredMajorVersion = 18;
 
-const currentNodeVersion = parseInt(process.versions.node.split('.')[0]);
+const currentNodeVersion = parseInt(process.versions?.node?.split('.')?.[0]);
 
 if (currentNodeVersion < requiredMajorVersion) {
-	console.error(
+	console.warn(
 		`You are running Node.js version ${currentNodeVersion}, but this application requires at least Node.js ${requiredMajorVersion}. Please upgrade your Node.js version.`
 	);
-	process.exit(1);
 }
 
 runCli();
