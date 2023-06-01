cbe9b808d62bc4087815404bf54d35949478ebb8
sejas <antonio@sejas.es>
2023-06-01 12:25:43 +0100

    wp-now: change warning text for lower node versions

--- packages/wp-now/src/main.ts
+++ packages/wp-now/src/main.ts
@@ -6,7 +6,7 @@ const currentNodeVersion = parseInt(process.versions?.node?.split('.')?.[0]);
 
 if (currentNodeVersion < requiredMajorVersion) {
 	console.warn(
-		`You are running Node.js version ${currentNodeVersion}, but this application requires at least Node.js ${requiredMajorVersion}. Please upgrade your Node.js version.`
+		`You are running Node.js version ${currentNodeVersion}, but this application recommends at least Node.js ${requiredMajorVersion} and isn't guaranteed to work on lower versions. Please upgrade your Node.js version.`
 	);
 }
 
