4cb8275d835bd3e1b877506c2352a86fe5f435fb
Adam Zieliński <adam@adamziel.com>
2024-01-16 15:00:58 +0100

    Render Playground after DOM load

--- packages/wordpress-playground-block/src/view.tsx
+++ packages/wordpress-playground-block/src/view.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import { createRoot } from '@wordpress/element';
 import PlaygroundPreview from './components/playground-preview';
 
-const playgroundDemo = Array.from(
-	document.getElementsByClassName('wordpress-playground-block')
-);
-
-for (const element of playgroundDemo) {
-	const rootElement = element as HTMLDivElement;
-	const root = createRoot(rootElement);
-	const attributes = JSON.parse(
-		atob(rootElement.dataset['attributes'] || '')
+function renderPlaygroundPreview() {
+	const playgroundDemo = Array.from(
+		document.getElementsByClassName('wordpress-playground-block')
 	);
 
-	root.render(<PlaygroundPreview {...attributes} />);
+	for (const element of playgroundDemo) {
+		const rootElement = element as HTMLDivElement;
+		const root = createRoot(rootElement);
+		const attributes = JSON.parse(
+			atob(rootElement.dataset['attributes'] || '')
+		);
+
+		root.render(<PlaygroundPreview {...attributes} />);
+	}
+}
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', renderPlaygroundPreview);
+} else {
+	renderPlaygroundPreview();
 }
