1d68c64ece37123931d3bef9acca9d38af448d66
bgrgicak <berislav.grgicak@gmail.com>
2024-04-05 12:00:28 +0200

    Playground plugin v.0.1.1

--- packages/playground/playground.php
+++ packages/playground/playground.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/WordPress/playground-tools/tree/trunk/packages/playground
  * Description: Packages your WordPress install and sends it to Playground.
  * Author: WordPress Contributors
- * Version: 0.1.0
+ * Version: 0.1.1
  * Requires PHP: 8.0
  * License: GPLv2
  * Text Domain: playground
