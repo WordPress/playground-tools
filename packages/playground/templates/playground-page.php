a1f8a72defbb345642193e9a43a6706dc9aa9260
Ronny Shani <ronnyshani@gmail.com>
2024-04-10 14:13:22 +0200

    Tweak copy and markup of top toolbar

--- packages/playground/templates/playground-page.php
+++ packages/playground/templates/playground-page.php
@@ -6,25 +6,25 @@ defined('ABSPATH') || exit;
 ?>
 <div id="wp-playground-wrapper">
     <div id="wp-playground-toolbar">
-        <span>
+        <p>
             <?php
             echo esc_attr(
                 sprintf(
                     // translators: %s: Site name.
                     __(
-                        'WordPress Playground preview for %s',
+                        'WordPress Playground preview of %s',
                         'playground'
                     ),
                     get_bloginfo('name')
                 )
             );
             ?>
-        </span>
+        </p>
         <a href="<?php echo esc_url(admin_url()); ?>" id="goBack">
-            <?php esc_attr_e('Go Back', 'playground'); ?>
+            <?php esc_attr_e('Back', 'playground'); ?>
         </a>
     </div>
     <div id="wp-playground-main-area">
-        <iframe credentialless id="wp-playground"></iframe>
+        <iframe credentialless id="wp-playground" title="WordPress Playground Sandbox"></iframe>
     </div>
 </div>
