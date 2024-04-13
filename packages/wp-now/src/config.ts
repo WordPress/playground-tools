87b7053e32353d5b9e12a8e31c22e820db12076b
sejas <antonio@sejas.es>
2024-04-13 10:43:16 +0100

    Open browser with blueprint landingPage instead of absoluteUrl

--- packages/wp-now/src/config.ts
+++ packages/wp-now/src/config.ts
@@ -45,6 +45,7 @@ export interface WPNowOptions {
 	numberOfPhpInstances?: number;
 	blueprintObject?: Blueprint;
 	reset?: boolean;
+	landingPage?: string;
 }
 
 export const DEFAULT_OPTIONS: WPNowOptions = {
@@ -55,6 +56,7 @@ export const DEFAULT_OPTIONS: WPNowOptions = {
 	mode: WPNowMode.AUTO,
 	numberOfPhpInstances: 1,
 	reset: false,
+	landingPage: '',
 };
 
 export interface WPEnvOptions {
@@ -166,6 +168,10 @@ export default async function getWpNowConfig(
 		if (siteUrl) {
 			options.absoluteUrl = siteUrl;
 			absoluteUrlFromBlueprint = siteUrl;
+		}
+		if (blueprintObject.landingPage) {
+			options.landingPage =
+				(await getAbsoluteURL()) + blueprintObject.landingPage;
 		}
 	}
 	return options;
