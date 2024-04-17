6b10700ba2ccb72d6a9b6db247469ef4b37438f6
Ronny Shani <ronnyshani@gmail.com>
2024-04-17 10:31:25 +0200

    Implement markup changes

--- packages/playground/templates/playground-page.php
+++ packages/playground/templates/playground-page.php
@@ -20,11 +20,11 @@ defined('ABSPATH') || exit;
             );
             ?>
         </p>
-        <a href="<?php echo esc_url(admin_url()); ?>" id="goBack">
+        <a href="<?php echo esc_url(admin_url()); ?>" id="goBack" class="button">
             <?php esc_attr_e('Back', 'playground'); ?>
         </a>
     </div>
     <div id="wp-playground-main-area">
-        <iframe credentialless id="wp-playground" title="WordPress Playground Sandbox"></iframe>
+        <iframe credentialless id="wp-playground" title="<?php _e('WordPress Playground Sandbox', 'playground'); ?>"></iframe>
     </div>
 </div>
